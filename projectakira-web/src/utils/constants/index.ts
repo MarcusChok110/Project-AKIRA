910b7b788a49563eb1ecf920853d33694a786d33
Marcus Chok <marcus.chok110@gmail.com>
2021-05-30 16:49:30 -0400

    Fix prod server url

--- projectakira-web/src/utils/constants/index.ts
+++ projectakira-web/src/utils/constants/index.ts
@@ -4,7 +4,7 @@
 export const __prod__ = process.env.NODE_ENV === 'production';
 
 export const SERVER_URL = __prod__
-  ? 'https://projectakira-api.herokuapp.com/'
+  ? 'https://projectakira-api.herokuapp.com'
   : 'http://localhost:5000';
 
 export const API_ROUTES = {
