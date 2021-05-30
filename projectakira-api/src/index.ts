919ac483d25ea6894290122b26d1ef28a25ac94c
Marcus Chok <marcus.chok110@gmail.com>
2021-05-30 15:44:11 -0400

    Add middleware to handle formdata

--- projectakira-api/src/index.ts
+++ projectakira-api/src/index.ts
@@ -2,8 +2,11 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import { schoolRouter } from './routers/schoolRouter';
+import multer from 'multer';
 
+// miscellaneous configuration
 dotenv.config();
+const upload = multer({ storage: multer.memoryStorage() });
 
 const PORT = process.env.PORT || 5000;
 const NODE_ENV = process.env.NODE_ENV;
@@ -13,6 +16,7 @@ const app = express();
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(upload.single('image'));
 app.use(cors());
 
 // Route controllers
